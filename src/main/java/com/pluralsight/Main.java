43f6d9df4548d45b96f55248985186a2cd583756
thejaylajones <janeissaj@gmail.com>
2025-04-26 20:18:55 -0400

    start of project

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+    // Accounting Ledger App Capstone
+
+    System.out.println("Welcome to The Doll Bank! \uD81B\uDE6D ");
+
+    // what is needed (HomeScreen)
+    //      Home Screen
+    //      - The home screen should give the user the following options. The
+    //        application should continue to run until the user chooses to exit.
+    //
+    //      Add Deposit
+    //      - prompt user for the deposit information and
+    //        save it to the csv file
+    //
+    //      Make Payment (Debit)
+    //      - prompt user for the debit
+    //        information and save it to the csv file
+    //
+    //      Ledger
+    //      - display the ledger screen
+    //
+    //      Exit
+    //      - exit the application
+
+
+
+
+
+
+    }
+}
