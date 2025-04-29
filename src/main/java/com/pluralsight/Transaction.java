c39517f06911442c56f12b37d0cd9641c840fa1e
thejaylajones <janeissaj@gmail.com>
2025-04-29 11:38:28 -0400

    update changes

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+public class Transaction {
+
+    // define variables needed
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    // add constructor
+    public  Transaction(String date, String time, String description, String vendor, double amount){
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+
+
+
+
+
+}
